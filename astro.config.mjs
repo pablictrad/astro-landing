9b317fa54dbe3daed8f5d0c8c26a5ea77f4392a9
Patricia Bustos <pablictrad@gmail.com>
2025-09-17 20:14:24 -0300

    deploy astro-landing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://pablictrad.github.io",
+  base: "/astro-landing",
+  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()]
   }
+   
 });
